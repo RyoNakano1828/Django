38d1ed534afe6ce5712efa85f3c7851b111e1581
ryo_nakano <naca.11868@gmail.com>
2021-02-26 18:54:48 +0900

    テンプレートを追加

--- project/urls.py
+++ project/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import tutorial_app.views as tutorial_app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', tutorial_app.index),
+    path('hello/', include('tutorial_app.urls')),
     path('<int:id>/<name>/<int:age>',tutorial_app.information)
 ]
--- tutorial_app/views.py
+++ tutorial_app/views.py
@@ -3,14 +3,33 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+# パラメータを受けてテンプレートを表示する
 def index(request):
-    if 'msg' in request.GET:
-        msg = request.GET['msg']
-        res = "Query Para:" + msg
+    if 'msg' and 'title' in request.GET:
+        title, msg = request.GET['title'], request.GET['msg']
+        params = {
+            'title': title,
+            'msg': msg
+        }
     else:
-        res = "Ellor request"
-    return HttpResponse(res)
+        params = {
+            'title': 'Hello Gjangooooo',
+            'msg': 'これはサンプルページで作ったテンプレートでーす'
+        }
+    params['goto'] = 'next'
+    params['numbers'] = range(5)
+    params['items'] = ['apple', 'ninnjin', 'daikon', 'tomato']
+    return render(request, 'hello/index.html', params)
 
+def next(request):
+    params = {
+        'title': 'hello/next',
+        'msg': 'これはサンプルページで作ったnextページでーす',
+        'goto': 'index'
+    }
+    return render(request, 'hello/index.html', params)
+
+# テンプレートを使わずViewだけで返すのと、URLを受けて表示する
 def information(request, id, name, age):
     res = "Your name: {}, Your id: {}, Your age: {}".format(name,id,age)
     return HttpResponse(res)
